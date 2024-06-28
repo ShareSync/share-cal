79a19ecc01bff80e333e7956199485b55fe413da
Fiyinfoluwa Afolayan <fiyinfoluwaafol@meta.com>
2024-06-28 15:04:47 -0700

    capturing and storing information from user into states

--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -1,17 +1,31 @@
 import "./LoginForm.css"
 import { Link } from 'react-router-dom';
+import { useState } from "react";
 
 
 function LoginForm (){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleOnSubmit (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        const userObj = {
+            email: email,
+            password: password
+        }
+        console.log("User Info: ", userObj);
+    }
+
     return(
         <div id="login-form">
             <form>
                 <h1>Login</h1>
-                <p>Email address</p>
-                <input type="email" placeholder="johndoe@email.com" />
+                <p>Email Address</p>
+                <input type="email" placeholder="johndoe@email.com" onChange={(e) => setEmail(e.target.value)} value={email}/>
                 <p>Password</p>
-                <input type="password" />
-                <button>Login</button>
+                <input type="password" onChange={(e) => setPassword(e.target.value)} value={password}/>
+                <button onClick={(e) => handleOnSubmit(e)}>Login</button>
                 <p>Don't have an account? <Link to='/signup'>Sign Up</Link></p>
             </form>
         </div>
--- frontend/src/components/SignUpForm/SignUpForm.jsx
+++ frontend/src/components/SignUpForm/SignUpForm.jsx
@@ -1,22 +1,37 @@
+import { useState } from "react";
 import "./SignUpForm.css"
 import { Link } from 'react-router-dom';
 
 function SignUpForm (){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+    function handleOnSubmit (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        const userObj = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password
+        }
+        console.log("User Info: ", userObj);
+    }
     return(
         <div id="login-form">
             <form>
                 <h1>Create New Account</h1>
                 <p>First Name</p>
-                <input type="input" placeholder="First Name" />
+                <input type="input" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)} value={firstName}/>
                 <p>Last Name</p>
-                <input type="input" placeholder="Last Name" />
-                <p>Email address</p>
-                <input type="email" placeholder="johndoe@email.com" />
+                <input type="input" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} value={lastName}/>
+                <p>Email Address</p>
+                <input type="email" placeholder="johndoe@email.com" onChange={(e) => setEmail(e.target.value)} value={email}/>
                 <p>Password</p>
-                <input type="password" />
-                <p>Confirm Password</p>
-                <input type="password" />
-                <button>Sign Up</button>
+                <input type="password" onChange={(e) => setPassword(e.target.value)} value={password}/>
+                <button onClick={(e) => handleOnSubmit(e)}>Sign Up</button>
                 <p>Already have an account? <Link to='/'>Login Here</Link></p>
             </form>
         </div>
