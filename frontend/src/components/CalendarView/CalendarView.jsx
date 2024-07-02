5c529b9d51afa9c6c1d0b5323db10163f8067577
Fiyinfoluwa Afolayan <fiyinfoluwaafol@meta.com>
2024-07-02 10:07:38 -0700

    API Logout integration in CalendarView.jsx

--- frontend/src/components/CalendarView/CalendarView.jsx
+++ frontend/src/components/CalendarView/CalendarView.jsx
@@ -1,10 +1,17 @@
 import "./CalendarView.css"
+import Header from "../Header/Header"
+import { UserContext } from '../../UserContext.js';
+import { useState, useContext } from "react";
 
 function CalendarView () {
+    const userInfo = useContext(UserContext).user;
+    console.log({userInfo});
     return (
-        <>
-            This is the Calendar View Page
-        </>
+            <>
+                <Header />
+                <p>This is the Calendar View Page</p>
+                <p>Welcome {userInfo.firstName}</p>
+            </>
     )
 }
 
